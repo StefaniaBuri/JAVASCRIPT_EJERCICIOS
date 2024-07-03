acba90970affa35d215b79d856d8cb7cecce020c
StefaniaBuri <60428937+StefaniaBuri@users.noreply.github.com>
2024-07-03 20:54:06 +0200

    Programado calculadora IMC

--- Ejercicio_006-calcularIMC/calculadora.js
+++ Ejercicio_006-calcularIMC/calculadora.js
@@ -1,9 +1,5 @@
  // calcular IMC
- function calcular(peso, altura) {
-    let nombre = document.getElementById("iNombre").innerHTML= nombre;
-    let altura = document.getElementById("iAltura").innerHTML= altura;
-    let peso = document.getElementById("iPeso").innerHTML= peso;
-    let imc = peso/((altura/100)**2);
+ function calcularIMC(peso, altura) {
+    let imc = Math.round(peso/((altura/100)**2));
     return imc;
 }
-alert("Tu indices es: "+ calcular());
